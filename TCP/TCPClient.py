d650a00331447fb41585bda9eee66e63e0fe7310
itallopacheco <60146868+itallopacheco@users.noreply.github.com>
2021-11-03 21:25:38 -0300

    Update TCPClient.py

--- TCP/TCPClient.py
+++ TCP/TCPClient.py
@@ -2,28 +2,28 @@
 # Importação da biblioteca de socket
 from socket import *
 
-nomeServidor = '127.0.0.1'
+nomeServidor = '192.0.2.10'
 
-portaServidor = 12000
+portaServidor = 80
 
 socketCliente = socket (AF_INET, SOCK_STREAM)
 
 socketCliente.connect ((nomeServidor, portaServidor))
 
 # Python 2
-# frase = raw_input ('Informe uma frase em letras minusculas: ')
+ frase = raw_input ('Informe uma frase em letras minusculas: ')
 
 # Python 3
-frase = input ('Informe uma frase em letras minusculas: ')
+#frase = input ('Informe uma frase em letras minusculas: ')
 
 socketCliente.send (frase.encode())
 
 fraseModificada = socketCliente.recv (1024)
 
 # Python 2
-# print "Do Servidor: ", fraseModificada
+ print "Do Servidor: ", fraseModificada
 
 # Python 3
-print ("Do Servidor: ", fraseModificada.decode())
+#print ("Do Servidor: ", fraseModificada.decode())
 
 socketCliente.close ()
